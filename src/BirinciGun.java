879f7a1da85e6ba2ac9cb823e3a4d0179b1d1405
umran78 <74472968+umran78@users.noreply.github.com>
2020-11-17 21:04:55 +0100

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -8,6 +8,7 @@ public class BirinciGun {
 		System.out.println("10:11 - Ikinci Gün");
 		System.out.println("10:26 - Ikinci Gun");
 		System.out.println("10:35 - Ikinci Gun");
+		System.out.println("11:03 - github.com");
 		
 	}
 
