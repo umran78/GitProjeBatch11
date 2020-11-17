83741d1b6cb9ac53d27e45c88c9551220c19ac5c
umran78 <helloholland8889@gmail.com>
2020-11-17 20:28:58 +0100

    Ikinci Gun 2. commit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -6,6 +6,7 @@ public class BirinciGun {
 		System.out.println("11:57 - Birinci Gün");
 		System.out.println("Ikinci Commit");
 		System.out.println("10:11 - Ikinci Gün");
+		System.out.println("10:26 - Ikinci Gun");
 		
 	}
 
