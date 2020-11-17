f9466a7142e0931a4efa5a27b92afc3527a8ac51
umran78 <helloholland8889@gmail.com>
2020-11-17 21:51:54 +0100

    mesaj

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -10,11 +10,8 @@ public class BirinciGun {
 		System.out.println("10:35 - Ikinci Gun");
         System.out.println("11:05 - local");
 		System.out.println("11:23 - github.com");
-<<<<<<< HEAD
-		System.out.println("ben buradan conflict beklemiyorum..");
-=======
-		System.out.println("11:28 - bakalım conflict olacak mı");
->>>>>>> refs/remotes/origin/master
+        System.out.println("ben buradan conflict beklemiyorum..");
+
 
 	}
 
