89ec704c3c43df890c495bd4191d60f57f48fee0
umran78 <74472968+umran78@users.noreply.github.com>
2020-11-18 20:42:00 +0100

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -8,10 +8,10 @@ public class BirinciGun {
 		System.out.println("10:11 - Ikinci Gün");
 		System.out.println("10:26 - Ikinci Gun");
 		System.out.println("10:35 - Ikinci Gun");
-        System.out.println("11:05 - local");
+                System.out.println("11:05 - local");
 		System.out.println("11:23 - github.com");
-        System.out.println("ben buradan conflict beklemiyorum..");
-        System.out.println("yepyeni bir satir");
+                System.out.println("ben buradan conflict beklemiyorum..");
+                System.out.println("yepyeni bir satir");
 
 
 	}
